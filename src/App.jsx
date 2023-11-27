e541513f97ff743ef0f940a9bd49ec91204de6a7
Negar <negar.amin7@gmail.com>
2023-11-27 17:50:51 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Select from './components/Select'
+import Input from './components/Input'
+import { units } from './units'
+
+function App() {
+  const [result, setResult] = useState(0)
+  const [info, setInfo] = useState({Amount:"", From:units[0].factor, To:units[0].factor})
+  const onChange = (e) =>{
+    setInfo(prevValue => ({...prevValue, [e.target.id.split("-")[0]]:e.target.value}))
+  }
+  const convert = () => {
+    let value = Number(info.Amount)*Number(info.From)/Number(info.To);
+    setResult(value)
+  }
+  return (
+    <>
+      <div className="converter-form">
+        <Input label={"Amount"} onChange={onChange}/>
+
+        <div className="row">
+          <Select label={"From"} onChange={onChange} items={units} />
+          <Select label={"To"} onChange={onChange} items={units} />
+          <button onClick={convert}>Convert</button>
+        </div>
+      </div>
+
+      <div id="result">
+        Result is: <span data-testid="result">{result}</span>
+      </div>
+    </>
+  )
+}
+
+export default App
